d48619128bd285b1a5f66eaa005e342d2b2e6bad
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2023-02-15 15:27:55 +0000

    Added extra label properties

--- src/xml_ui/controls/Label.java
+++ src/xml_ui/controls/Label.java
@@ -1,10 +1,12 @@
 package xml_ui.controls;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JLabel;
 
 import xml_ui.attributes.SetterAttribute;
+import xml_ui.exceptions.InvalidXMLException;
 
 public class Label extends JLabel
 {
@@ -30,5 +32,39 @@ public class Label extends JLabel
     public void SetForeground(String colour)
     {
         setForeground(Color.decode(colour));
+    }
+
+    @SetterAttribute("Weight")
+    public void SetWeight(String weight) throws InvalidXMLException
+    {
+        switch (weight)
+        {
+            case "BoldItalic":
+                setFont(getFont().deriveFont(getFont().getStyle() | Font.BOLD | Font.ITALIC));
+                break;
+            case "Bold":
+                setFont(getFont().deriveFont(getFont().getStyle() | Font.BOLD));
+                break;
+            case "Italic":
+                setFont(getFont().deriveFont(getFont().getStyle() | Font.ITALIC));
+                break;
+            case "Normal":
+                setFont(getFont().deriveFont(getFont().getStyle() & ~Font.BOLD & ~Font.ITALIC));
+                break;
+            default:
+                throw new InvalidXMLException("Invalid weight '" + weight + "'.");
+        }
+    }
+
+    @SetterAttribute("Size")
+    public void SetSize(String size) throws InvalidXMLException
+    {
+        setFont(getFont().deriveFont(Float.parseFloat(size)));
+    }
+
+    @SetterAttribute("Font")
+    public void SetFont(String font) throws InvalidXMLException
+    {
+        setFont(new Font(font, getFont().getStyle(), getFont().getSize()));
     }
 }
