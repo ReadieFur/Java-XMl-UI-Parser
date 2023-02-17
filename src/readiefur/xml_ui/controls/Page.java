587ba8f491c5062a9179121478549c518b3629b8
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2023-02-17 18:13:09 +0000

    Create Page.java

---
+++ src/readiefur/xml_ui/controls/Page.java
@@ -0,0 +1,16 @@
+package readiefur.xml_ui.controls;
+
+import java.awt.GridBagLayout;
+
+import javax.swing.JPanel;
+
+public class Page extends JPanel
+{
+    public Page()
+    {
+        super();
+
+        setLayout(new GridBagLayout());
+        setOpaque(false);
+    }
+}
